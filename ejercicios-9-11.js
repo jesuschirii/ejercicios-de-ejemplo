2297157f9123431fb8e4f3396245252423836889
Jesus Chirinos <jesuschirii@gmail.com>
2021-01-03 23:41:17 -0400

    Add files via upload

---
+++ ejercicios-9-11.js
@@ -0,0 +1,40 @@
+/* 9) Programa una función que obtenga un numero aleatorio entre 
+501 y 600 */
+
+/* const nA=(n1=501,n2=600,a) =>{
+    a=Math.round(Math.random()*((600)-501)+501);
+    console.log(a);
+}
+nA()
+ */
+
+ 
+/* 10) Programa una función que reciba un número y evalúe si es capicúa o no 
+ (que se lee igual en un sentido que en otro), 
+pe. miFuncion(2002) devolverá true  */
+
+
+/* const cC=(n="",n1,n2)=>{
+n1=n.toString();n2=parseInt(n1.split("").reverse().join(""))
+if(n2===n){
+    console.log("Es una capicua");
+}else {console.log("No es capicua");}
+}
+cC(202)
+ */
+
+
+
+/* 11) Programa una función que calcule el factorial de un número 
+(El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), 
+pe. miFuncion(5) devolverá 120 */
+
+
+/* const fT=(n="",v=1)=>{
+for(let i=1;i<n+1;i++){
+v=v*i;
+}
+console.log(v);
+}
+fT(4)
+ */
